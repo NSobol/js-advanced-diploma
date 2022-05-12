c994b6cd44a14ae6517a40800a32eda7ae90d539
Soboleva Natalia <nsoboleva1979@rambler.ru>
2022-05-12 18:19:23 +0300

    Add tests

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,3 +1,10 @@
+import Bowman from './Characters/Bowman';
+import Daemon from './Characters/Daemon';
+import Swordman from './Characters/Swordman';
+import Magician from './Characters/Magician';
+import Undead from './Characters/Undead';
+import Vampire from './Characters/Vampire';
+import cursors from './cursors';
 import themes from './themes';
 import Team from './Team';
 import GamePlay from './GamePlay';
@@ -8,12 +15,17 @@ export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.gameState = new GameState();
+    this.userTeam = new Team();
+    this.compTeam = new Team();
+    this.compCharacters = [Daemon, Undead, Vampire];
+    this.userCharacters = [Bowman, Swordman, Magician];
   }
 
   init() {
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
-    this.gamePlay.drawUi(themes[this.GameState]);
+    this.gamePlay.drawUi(themes[this.gameState.level]);
   }
 
   onCellClick(index) {
